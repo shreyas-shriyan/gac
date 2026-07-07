cc3c6ada1af982727e9dda8305f06b116de9cb35
shreyas-shriyan <shreyshriyan@gmail.com>
2026-07-07 11:45:21 +0000

    added list and create profile

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use serde::{Deserialize, Serialize};
 use std::{env, fs};
 
-enum Command {
-    list,
-}
-
 // Define structs that match the TOML structure
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize)]
 struct AppConfig {
     profile: Vec<Config>,
 }
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Deserialize, Serialize)]
 struct Config {
     nickname: String,
     username: String,
@@ -36,6 +32,20 @@ fn get_config_path() -> std::path::PathBuf {
     return path;
 }
 
+// List Functions
+fn list_profiles() {
+    let toml_string = fs::read_to_string(get_config_path()).expect("Failed to read config.toml");
+    let app_config: AppConfig = toml::from_str(&toml_string).expect("Failed to parse TOML");
+
+    for profile in app_config.profile {
+        println!(
+            "Nickname: {}, Username: {}",
+            profile.nickname, profile.username
+        );
+    }
+}
+
+// Save Functions
 fn save_config(app_config: &AppConfig) {
     // Convert the struct back to a formatted TOML string
     let toml_string = toml::to_string(app_config).expect("Failed to serialize to TOML");
@@ -50,33 +60,62 @@ fn save_config(app_config: &AppConfig) {
     fs::write(path, toml_string).expect("Failed to write to file");
 }
 
+fn add_profile(nickname: String, username: String, email: String) {
+    let mut app_config = if get_config_path().exists() {
+        let toml_string =
+            fs::read_to_string(get_config_path()).expect("Failed to read config.toml");
+        toml::from_str(&toml_string).expect("Failed to parse TOML")
+    } else {
+        AppConfig {
+            profile: Vec::new(),
+        }
+    };
+
+    let new_profile = Config {
+        nickname,
+        username,
+        email,
+    };
+
+    // check if the nickname or email already exists in the profiles
+    for profile in &app_config.profile {
+        if profile.nickname == new_profile.nickname {
+            println!("Nickname '{}' already exists.", new_profile.nickname);
+            return;
+        }
+        if profile.email == new_profile.email {
+            println!("Email '{}' already exists.", new_profile.email);
+            return;
+        }
+    }
+
+    app_config.profile.push(new_profile);
+    save_config(&app_config);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 1 {
-        println!("First CLI argument: {}", args[1]);
-    } else {
+    if args.len() < 2 {
         println!("No CLI arguments provided.");
+        println!("Choose a command: list , add , remove");
+        return;
     }
 
-    // let profile = AppConfig {
-    //     profile: vec![
-    //         Config {
-    //             nickname: String::from("personal"),
-    //             username: String::from("shreyas-shriyan"),
-    //             email: String::from("shreyshriyan@gmail.com"),
-    //         },
-    //         Config {
-    //             nickname: String::from("work"),
-    //             username: String::from("shre"),
-    //             email: String::from("shreyshriyan@gmail.com"),
-    //         },
-    //     ],
-    // };
+    let command = args[1].as_str();
 
-    //   save_config(&profile);
-
-    let toml_string = fs::read_to_string(get_config_path()).expect("Failed to read config.toml");
-
-    println!("TOML String: {}", toml_string);
+    match command {
+        "list" => list_profiles(),
+        "add" => {
+            if args.len() < 5 {
+                println!("Usage: add <nickname> <username> <email>");
+                return;
+            }
+            let nickname = args[2].clone();
+            let username = args[3].clone();
+            let email = args[4].clone();
+            add_profile(nickname, username, email);
+        }
+        _ => println!("Unknown command: {}", command),
+    }
 }
